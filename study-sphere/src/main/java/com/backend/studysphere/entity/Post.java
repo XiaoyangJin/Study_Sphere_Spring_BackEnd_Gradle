c12906334ffea84fb44078173c1a6062bf5ec4c8
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-29 12:02:13 -0400

    Create Post Entity with id as PK

---
+++ study-sphere/src/main/java/com/backend/studysphere/entity/Post.java
@@ -0,0 +1,59 @@
+package com.backend.studysphere.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    private int post_id;
+    private String title;
+    private String summary;
+    private String main_content;
+    public int getPost_id() {
+        return post_id;
+    }
+
+    // No-argument constructor
+    public Post() {
+    }
+
+    public void setPost_id(int post_id) {
+        this.post_id = post_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getMain_content() {
+        return main_content;
+    }
+
+    public void setMain_content(String main_content) {
+        this.main_content = main_content;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "post_id=" + post_id +
+                ", title='" + title + '\'' +
+                ", summary='" + summary + '\'' +
+                ", main_content='" + main_content + '\'' +
+                '}';
+    }
+}
