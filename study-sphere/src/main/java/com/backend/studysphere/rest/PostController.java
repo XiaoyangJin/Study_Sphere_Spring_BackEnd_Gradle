db49bea3f61fd5206533dda2c4dc74ae96f157ed
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-05 13:55:07 -0400

    Add Get API

--- study-sphere/src/main/java/com/backend/studysphere/rest/PostController.java
+++ study-sphere/src/main/java/com/backend/studysphere/rest/PostController.java
@@ -5,10 +5,9 @@ import com.backend.studysphere.entity.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/posts")
@@ -20,5 +19,11 @@ public class PostController {
     public ResponseEntity<Post> createPost(@RequestBody Post post){
         Post savedPost = postRepository.save(post);
         return new ResponseEntity<>(savedPost, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Post>> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+        return new ResponseEntity<>(posts, HttpStatus.OK);
     }
 }
