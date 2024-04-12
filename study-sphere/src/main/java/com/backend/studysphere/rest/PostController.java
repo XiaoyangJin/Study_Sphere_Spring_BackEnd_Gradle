3b3a9024a62264da0ee2533f599f529cdc6e7d21
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-12 19:05:03 -0400

    Add get post by id api

--- study-sphere/src/main/java/com/backend/studysphere/rest/PostController.java
+++ study-sphere/src/main/java/com/backend/studysphere/rest/PostController.java
@@ -1,6 +1,7 @@
 package com.backend.studysphere.rest;
 
 import com.backend.studysphere.dao.PostRepository;
+import com.backend.studysphere.entity.Account;
 import com.backend.studysphere.entity.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,6 +21,13 @@ public class PostController {
     public ResponseEntity<Post> createPost(@RequestBody Post post){
         Post savedPost = postRepository.save(post);
         return new ResponseEntity<>(savedPost, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/posts/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable long id) {
+        return postRepository.findById(id)
+                .map(post -> new ResponseEntity<>(post, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping
