a46d62b9c1056432d50e930ca408d9a0f4d78f4b
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-04-12 18:41:52 -0400

    Add post api and update lastlogin time

--- study-sphere/src/main/java/com/backend/studysphere/rest/AccountController.java
+++ study-sphere/src/main/java/com/backend/studysphere/rest/AccountController.java
@@ -1,9 +1,16 @@
 package com.backend.studysphere.rest;
 
 import com.backend.studysphere.dao.AccountRepository;
+import com.backend.studysphere.entity.Account;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
 
 @RestController
 @RequestMapping("api/accounts")
@@ -11,4 +18,10 @@ public class AccountController {
 
     @Autowired
     private AccountRepository accountRepository;
+
+    public ResponseEntity<Account> createAccount(@RequestBody Account account) {
+        account.setLastLoginTime(OffsetDateTime.now()); // Setting the last login time at account creation
+        Account savedAccount = accountRepository.save(account);
+        return new ResponseEntity<>(savedAccount, HttpStatus.CREATED);
+    }
 }
