6cd4717de4253b22c1e21307039a8cf275dad89a
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-14 18:21:58 -0400

    Initialize User entity

---
+++ studysphere/src/main/java/com/v2/studysphere/entity/User.java
@@ -0,0 +1,39 @@
+package com.v2.studysphere.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "v2users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_posts",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "post_id")
+    )
+    private Set<Post> posts = new HashSet<>();
+}
