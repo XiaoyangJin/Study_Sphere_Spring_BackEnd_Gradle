18770f761fe7e54747c616e35e7e627d4ce4f956
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-14 18:52:41 -0400

    Finish all impl for User controller

---
+++ studysphere/src/main/java/com/v2/studysphere/controller/UserController.java
@@ -0,0 +1,66 @@
+package com.v2.studysphere.controller;
+
+import com.v2.studysphere.entity.User;
+import com.v2.studysphere.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/v2user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> user = userService.findById(id);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        Optional<User> user = userService.findById(id);
+        if (user.isPresent()) {
+            User updatedUser = user.get();
+            updatedUser.setUsername(userDetails.getUsername());
+            updatedUser.setEmail(userDetails.getEmail());
+            updatedUser.setPasswordHash(userDetails.getPasswordHash());
+            return ResponseEntity.ok(userService.save(updatedUser));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        if (userService.findById(id).isPresent()) {
+            userService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
