99772cff36e2f34acfd7438a0517facb9fd34c6b
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-14 18:45:42 -0400

    Implement post service

---
+++ studysphere/src/main/java/com/v2/studysphere/service/impl/PostServiceImpl.java
@@ -0,0 +1,40 @@
+package com.v2.studysphere.service.impl;
+
+import com.v2.studysphere.entity.Post;
+import com.v2.studysphere.repository.PostRepository;
+import com.v2.studysphere.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostServiceImpl(PostRepository postRepository){
+        this.postRepository = postRepository;
+    }
+    @Override
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    @Override
+    public Optional<Post> findById(Long id) {
+        return postRepository.findById(id);
+    }
+
+    @Override
+    public Post save(Post post) {
+        return postRepository.save(post);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        postRepository.deleteById(id);
+    }
+}
