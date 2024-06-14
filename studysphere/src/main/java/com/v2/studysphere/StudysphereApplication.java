2d20b106e0dc166c5f14c5b6a8f4f5bebc6ba1ed
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-14 17:40:33 -0400

    Add v2 version

---
+++ studysphere/src/main/java/com/v2/studysphere/StudysphereApplication.java
@@ -0,0 +1,13 @@
+package com.v2.studysphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudysphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudysphereApplication.class, args);
+	}
+
+}
---
+++ studysphere/src/test/java/com/v2/studysphere/StudysphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v2.studysphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudysphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
