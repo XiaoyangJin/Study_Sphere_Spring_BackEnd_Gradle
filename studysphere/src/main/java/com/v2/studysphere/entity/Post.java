79a23a83ee700f0b2f867acb2c3c0866ba8fe074
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-14 18:24:23 -0400

    Initialize Post entity

---
+++ studysphere/src/main/java/com/v2/studysphere/entity/Post.java
@@ -0,0 +1,26 @@
+package com.v2.studysphere.entity;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "v2posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column
+    private String summary;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToMany(mappedBy = "posts")
+    private Set<User> users = new HashSet<>();
+}
